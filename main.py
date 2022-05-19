cf91867c3966dfe66341ff258dfaf2d46c7773ed
Evgeniy <evgeniy.eg1@yandex.ru>
2022-05-19 11:51:40 +0200

    Add distance energy to optimisation process. Add energy visualisation. Integrate with the W&B

--- main.py
+++ main.py
@@ -3,12 +3,32 @@ import matplotlib.pyplot as plt
 import cv2
 import pandas as pd
 import skimage
+import wandb
 
 from utils import *
 
 VERBOSE = False
-np.random.seed(2022)
+USE_WANDB = True
 
+random_seed = 2022
+lr = 1.
+lambda_dist = 0.5 * lr
+lambda_blob = 0.5 * lr
+dist_alpha, dist_sigma = 1., 1.
+dist_n_neighbours = 3
+
+if USE_WANDB:
+    wandb.init(project='cones_AOSLO', config={
+        'lr': lr,
+        'lambda_dist': lambda_dist,
+        'lambda_blob': lambda_blob,
+        'dist_alpha': dist_alpha,
+        'dist_sigma': dist_sigma,
+        'dist_n_neighbours': dist_n_neighbours,
+        'random_seed': random_seed,
+    })
+
+np.random.seed(random_seed)
 
 data = pd.read_csv('dataset/BAK1008L1_2020_07_02_11_56_18_AOSLO_788_V006_annotated_JLR_128_97_646_612.csv')
 img = cv2.imread('dataset/BAK1008L1_2020_07_02_11_56_18_AOSLO_788_V006_annotated_JLR_128_97_646_612.tiff', -1)
@@ -22,12 +42,12 @@ if VERBOSE:
 ### CALC GRADINENT OVER IMG ###
 grad = calc_grad(img)
 if VERBOSE:
-    plt.imshow(grad, cmap = 'gray')
+    plt.imshow(grad, cmap='gray')
     plt.show()
 
 ### CREATE PARTICLE LOCATIONS ON IMG ###
 n_particles = 1000
-particles = np.random.uniform(low=[0,0], high=[img.shape[0], img.shape[1]], size=(n_particles,2))
+particles = np.random.uniform(low=[0, 0], high=[img.shape[0], img.shape[1]], size=(n_particles, 2))
 
 ### SHOW PARTICLE LOCATIONS ON IMG ###
 if VERBOSE:
@@ -35,7 +55,7 @@ if VERBOSE:
 
 ### GET PARTICLE DIST ENERGY ###
 n_neighbours = 2
-particles_dist_energy = calc_dist_energy(particles, n_neighbours)
+# particles_dist_energy = calc_dist_energy(particles, n_neighbours)
 
 ### GET PARTICLE BLOBNESS ENERGY ###
 hessian_m = skimage.feature.hessian_matrix(img, sigma=1, mode='constant', cval=0, order='rc')
@@ -44,31 +64,56 @@ eigs = skimage.feature.hessian_matrix_eigvals(hessian_m)
 # pointwise division, multiplications
 # page 5(down) from "Multiscale Vessel Enhancement Filtering"
 # R_b - measure of "blobness"
-R_b = np.divide( np.abs(eigs[0,:,:]), np.abs(eigs[1,:,:]) )
+R_b = np.divide(np.abs(eigs[0, :, :]), np.abs(eigs[1, :, :]))
 
 # calc grad of "Blobness field"
 blobness_grad = calc_grad_field(R_b)
 print(blobness_grad.shape)
 
-visualize(img_colorful, particles, is_save=True,
-          img_name='start', save_dir='./examples/')
-lr = 1.
-lambda_dist = 0.5 * lr
-lambda_blob = 0.5 * lr
+if VERBOSE:
+    visualize(img_colorful, particles, is_save=True,
+              img_name='start', save_dir='./examples/')
 
-for i in range(2000):
+for iteration in range(200):
+
+    exp_resulting_vectors, exp_resulting_vectors_modules = calc_dist_energy(particles,
+                                                                            n_neighbours=dist_n_neighbours,
+                                                                            alpha=dist_alpha,
+                                                                            sigma=dist_sigma
+                                                                            )
+
     for particle_idx, particle in enumerate(particles):
-        particle[0] += -1 * lambda_dist * blobness_grad[int(particle[0])][int(particle[1])][0]
-        particle[0] = bicycle(particle[0])
-        particle[1] += -1 * lambda_dist * blobness_grad[int(particle[0])][int(particle[1])][1]
+        particle[0] += -1 * lambda_blob * blobness_grad[int(particle[0])][int(particle[1])][0] + \
+                       lambda_dist * exp_resulting_vectors[particle_idx][0]
+        particle[0] = bicycle(particle[0])  # to stay inside image borders
+        particle[1] += -1 * lambda_dist * blobness_grad[int(particle[0])][int(particle[1])][1] + \
+                       lambda_dist * exp_resulting_vectors[particle_idx][1]
         particle[1] = bicycle(particle[1])
 
-    if i % 100 == 0:
-        visualize(img_colorful, particles, is_save=True,
-                  img_name='step_'+str(i), save_dir='./examples/')
 
-visualize(img_colorful, particles, is_save=True,
-          img_name='finish', save_dir='./examples/')
+
+    if USE_WANDB:
+        blob_energy = calc_blob_energy(R_b, particles)
+        wandb.log({'step': iteration,
+                   'blob_energy_sum': np.sum(blob_energy),
+                   'blob_energy_mean': np.mean(blob_energy),
+                   'dist_energy_sum': np.sum(exp_resulting_vectors_modules),
+                   'dist_energy_mean': np.mean(exp_resulting_vectors_modules),
+                   'dist_energy_hist': exp_resulting_vectors_modules})
+        # wandb.log({'step': iteration, 'blob_energy_mean': np.mean(blob_energy)})
+        # wandb.log({'step': iteration, 'dist_energy': calc_dist_energy(particles, n_neighbours=1)})
+    else:
+
+        print('blob_energy= ', np.mean(calc_blob_energy(R_b, particles)))
+        print('dist_energy= ', np.sum(exp_resulting_vectors_modules))
+
+    if iteration % 100 == 0 and VERBOSE:
+        visualize(img_colorful, particles, is_save=True,
+                  img_name='step_' + str(iteration), save_dir='./examples/')
+
+if VERBOSE:
+    visualize(img_colorful, particles, is_save=True,
+              img_name='finish', save_dir='./examples/')
 
 # if VERBOSE:
 # plt.imshow(R_b, cmap = 'gray')
--- utils.py
+++ utils.py
@@ -4,15 +4,21 @@ import cv2
 import pandas as pd
 from scipy.spatial import distance_matrix
 
+
 def bicycle(val, min_val=0, max_val=515):
-    val = val if val < max_val else max_val-1.
-    val = val if val > min_val else min_val+1
+    val = val if val < max_val else max_val - 1.
+    val = val if val > min_val else min_val + 1
     return val
+
 
 def metric():
     pass
 
-def calc_dist_energy(particles, n_neighbours):
+
+def calc_distances(particles, n_neighbours):
+    def get_particle_xy(idx):
+        return particles[idx]
+
     n_particles = particles.shape[0]
     ### FIND NEAREST NEIGHBOURS, CALC DISTANCES ###
     dist_matr = distance_matrix(particles, particles)
@@ -20,33 +26,62 @@ def calc_dist_energy(particles, n_neighbours):
     neigh_ind = np.argsort(dist_matr, axis=1)[:, 1:n_neighbours + 1]
 
     top_dist_matr = np.zeros((n_particles, n_neighbours))
+    top_dist_vectors = np.zeros((n_particles, n_neighbours, 2))
     for particle_idx, particle_distances in enumerate(dist_matr):
         for idx_of_closest_neighbour in range(n_neighbours):
-            top_dist_matr[particle_idx][idx_of_closest_neighbour] = dist_matr[particle_idx][
-                neigh_ind[particle_idx][idx_of_closest_neighbour]]
+            cur_neighbour_idx = neigh_ind[particle_idx][idx_of_closest_neighbour]
+            top_dist_matr[particle_idx][idx_of_closest_neighbour] = dist_matr[particle_idx][cur_neighbour_idx]
 
-    ### CALC DISTANCE ENERGY ###
-    distance_energy = np.sum(top_dist_matr, axis=1)
-    return distance_energy
+            cur_neighbour_coord = particles[cur_neighbour_idx]
+            cur_particle_coord = particles[particle_idx]
+            top_dist_vectors[particle_idx][idx_of_closest_neighbour] = cur_particle_coord - cur_neighbour_coord
 
-def blobness_energy():
-    pass
+    return top_dist_matr, top_dist_vectors
+
+
+def calc_dist_energy(particles, n_neighbours, alpha, sigma):
+    def exp_vector_norm(x,y):
+        return np.sqrt((x ** 2.) + (y ** 2.)) / (sigma ** 2.)
+
+    top_dist_matr, top_dist_vectors = calc_distances(particles, n_neighbours)
+
+    exp_resulting_vectors = np.zeros((top_dist_vectors.shape[0], 2))
+    for vector_idx, neigh_vectors in enumerate(top_dist_vectors):
+        directions = []
+        for vector in neigh_vectors:
+            directions.append( vector / exp_vector_norm(*vector) )
+        directions = np.asarray(directions)
+        exp_resulting_vectors[vector_idx] = alpha * np.sum(directions, axis=0)
+
+    exp_resulting_vectors_modules = np.linalg.norm(exp_resulting_vectors)
+    return exp_resulting_vectors, exp_resulting_vectors_modules
+
+
+def calc_blob_energy(R_b, particles):
+    blob_energy = np.zeros(particles.shape[0])
+    for idx, (x_pos, y_pos) in enumerate(particles):
+        blob_energy[idx] = R_b[int(x_pos)][int(y_pos)]
+    return blob_energy
+
 
 def calc_grad(img):
     laplacian = cv2.Laplacian(img, cv2.CV_64F)
     return laplacian
+
 
 def calc_grad_field(img):
     gx, gy = np.gradient(img)
     grad_vector = np.stack((gx, gy), axis=2)
     return grad_vector
 
+
 def visualize(img_colorful, particles, is_save=False, img_name='test', save_dir='./examples/'):
     image_particles = img_colorful.copy()
     for particle in particles:
         # print((particle[0], particle[1]))
-        cv2.circle(image_particles, (round(particle[0]), round(particle[1])), radius=1, color=(0, 255, 255), thickness=-1)
+        cv2.circle(image_particles, (round(particle[0]), round(particle[1])), radius=1, color=(0, 255, 255),
+                   thickness=-1)
     if is_save:
-        cv2.imwrite(save_dir+img_name+'.jpg', image_particles)
+        cv2.imwrite(save_dir + img_name + '.jpg', image_particles)
     cv2.imshow('img with particles', image_particles)
     cv2.waitKey()
